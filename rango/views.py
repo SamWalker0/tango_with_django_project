5ee0ea8c91dc8d48f0e2b11afba158cee32001e2
Sam Walker <2379093w@student.gla.ac.uk>
2020-01-30 20:32:01 +0000

    completed Chapter 4 exercises

--- rango/views.py
+++ rango/views.py
@@ -3,4 +3,11 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Rango says hey there partner")
+    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+    
+    return render(request, 'rango/index.html', context=context_dict)
+    
+def about(request):
+    context_dict = {'name': 'Sam'}
+    
+    return render(request, 'rango/about.html', context=context_dict)
